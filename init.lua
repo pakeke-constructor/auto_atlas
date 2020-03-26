5f9d6b44c1a6c0ac827db52c996d59dcee224a0d
pakeke-constructor <62230683+pakeke-constructor@users.noreply.github.com>
2020-03-26 18:57:34 +1300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
-local PATH = (...):gsub('%.[^%.]+$',"" )
+
+local PATH = (...):gsub('%.[^%.]+$','')
 local binpack = require(PATH..".binpack")
 
 
@@ -9,36 +10,73 @@ local atlas = {}
 local atlas_mt = { __index = atlas }
 
 atlas.new = function(x, y)
-    local x = x or 2048
-    local y = y or 2048
+    x = x or 2048
+    y = y or 2048
     return setmetatable({
-        width, height = x, y,
+        width = x, height = y,
         binpack = binpack(x, y),
-        canvas = lg.newCanvas(x, y)
+        image = love.image.newImageData(x, y),
+        path = ""
     }, atlas_mt)
+end
+
+
+function atlas:draw(quad, x, y, r, sx, sy, ox, oy, kx, ky )
+    lg.draw( self.image, quad, x, y, r, sx, sy, ox, oy, kx, ky )
 end
 
 function atlas:add(sprite, quad)
     assert(sprite:typeOf("image"), " atlas:add( image, [quad] )  expected image to be of type \n Image. instead, got type:  "..tostring(type(sprite)))
+    lg.push()
+    lg.reset()
 
-    local width = sprite:getWidth()
-    local height = sprite:getHeight()
 
+    if type(sprite) == "string" then
+        local sprite_path = self.path..sprite
+        sprite = lg.newImage(sprite_path)
+        local width,height = sprite:getWidth(), sprite:getHeight()
+        local new = self.binpack:insert(width+1, height+1)
+        -- Converting Image to Canvas:
+        local temp_canvas = lg.newCanvas(width, height)
+        temp_canvas:draw(sprite)
+        -- Canvas to ImageData:
+        local img_data = temp_canvas:getImageData()
+        -- ImageData w/ Image:replacePixels
+        self.image:replacePixels(img_data, nil, 1, new.x, new.y)
+        lg.pop()
+        return lg.newQuad(new.x, new.y, width, height, self.width, self.height)
+    end
+
+    -- Is image  +  quad.
     if quad then
         assert(quad:typeOf("quad"), " atlas:add( image, [quad] )  expected optional arg [quad] to be of type \n quad. instead, got type:  "..tostring(type(quad)))
+        local _, _, width, height = quad:getViewport()
         local new = self.binpack:insert(width+1, height+1)
-        lg.setCanvas(self.canvas)
-            lg.draw(sprite, quad, new.x, new.y)
-        lg.setCanvas()
+        -- Converting Image to Canvas:
+        local temp_canvas = lg.newCanvas(width, height)
+        temp_canvas:draw(sprite, quad)
+        local img_data = temp_canvas:getImageData()
+        -- ImageData w/ Image:replacePixels
+        self.image:replacePixels(img_data, nil, 1, new.x, new.y)
+        lg.pop()
         return lg.newQuad(new.x, new.y, width, height, self.width, self.height)
+
+    -- Is image.
     else
+        local width, height = sprite:getWidth(), sprite:getHeight()
         local new = self.binpack:insert(width+1, height+1)
-        lg.setCanvas(self.canvas)
-            lg.draw(sprite, new.x, new.y)
-        lg.setCanvas()
+        -- Converting Image to Canvas:
+        local temp_canvas = lg.newCanvas(width, height)
+        temp_canvas:draw(sprite)
+        -- Canvas to ImageData:
+        local img_data = temp_canvas:getImageData()
+        -- ImageData w/ Image:replacePixels
+        self.image:replacePixels(img_data, nil, 1, new.x, new.y)
+        lg.pop()
         return lg.newQuad(new.x, new.y, width, height, self.width, self.height)
     end
 end
 
-return setmetatable( atlas, {__call = atlas.new} )
+return setmetatable(atlas, {__call = atlas.new})
+
 
